01fbeb7966967acbc2cb5f89aaf0c696e71efa3f
yuan_horizon <hejun.y@wustl.edu>
2024-11-06 09:58:40 -0600

    11.5 update

--- HW_2/Nov_6_HW_2.py
+++ HW_2/Nov_6_HW_2.py
@@ -1,10 +1,10 @@
 import pandas as pd                     # To load data, we use the package pandas
 import numpy as np
 import matplotlib.pyplot as plt
-
 import cvxopt                 # install it first by running: "pip install cvxopt" via Spyder
 from cvxopt import matrix, solvers
 from numpy.ma.core import shape
+from  Horizon_Tool.Data_Cal import *
 
 df = pd.read_excel('../Data/Factors_July26_July11.xlsx')        # It has 5 columns:  date, mkt, size, b/m, riskree rate
 df2 = pd.read_excel('../Data/Indu10_July26_July11.xlsx')       # return on 10 industry portfolios from Ken French's web
@@ -19,7 +19,7 @@ T = len(df)  # The number of observations
 Re = np.ones((T, 10))  # creat storage for excess returns
 for i in range(10):
     Re[:, i] = R1[:, i] - rf  # the excess return:  each indu substracts riskfree rate, Re[:,i]-rf
-print(shape(Re))
+# print(shape(Re))
 mu10 = np.mean(Re, axis=0)  # the mean taking each column of the matrix, a row vector of 10
 # print(f"before {mu10}")
 mu10 = mu10.T  # make it a column vector
@@ -29,9 +29,10 @@ VI = np.linalg.inv(V10)  # The inverse of V
 # The optimal weights on the 5 risky aasets
 gamma = 3  # The risk-averse coeff.
 w10 = (1 / gamma) * VI @ mu10  # an alternative:  np.matmul(VI, mu10)
-
-
+con_w = []
+no_con_w = []
 def q_one_1():
+    global con_w, no_con_w
     # Compute the the Opt Port under bound constraints
     Q = gamma * V10  # the mapping of utility paramters into quadratic programming
     # print(f"previous {Q}")
@@ -47,20 +48,42 @@ def q_one_1():
     # print(h)
     G1 = np.eye(10)  # identiy matrix of order 10
     G2 = - np.eye(10)
-    print(G2)
+    # print(G2)
     G = np.append(G1, G2, axis=0)
     G = matrix(G)
-    print(G1)
-    print(G)
+    # print(G1)
+    # print(G)
     solvers.options['show_progress'] = False  # this prevent print progress data
     sol = solvers.qp(Q, q, G, h)  # Format:  solvers.qp(Q, q, G, h, A, b)
-    sola = np.array(sol['x'])
+    con_w = np.array(sol['x'])
     print('The Optimal wights on the 10 assets with constraints: 0 <=   <=.4 \n')
     np.set_printoptions(formatter={'float': '{: 0.4f}'.format})
-    print(sola.T)
+    # print(f"sola is {con_w.T}")
     sol1 = solvers.qp(Q, q)  # using the solver for the unconstrained solution
-    solb = np.array(sol1['x'])
+    no_con_w = np.array(sol1['x'])
     print('\n The Optimal wights on the 10 assets with no constraints, using Solvers instead of formula  \n ')
-    print(solb.T)
+    print(no_con_w.T)
+    print(type(no_con_w.T))
 
 q_one_1()
+def q_one_2():
+    Port = np.ones((T,))  # define this T-vector to store the returns on the portfolio
+    # to be compatible with rf
+    Port[0] = np.dot(con_w.T, Re[0]) + rf[0]  # return in the first period, the weight on rf is absorbed
+    # into the previous excess return term, see formulas in the slides
+    for t in range(T):
+        Port[t] = np.dot(con_w.T, Re[t]) + rf[t]
+    ExPort = Port - rf  # excess return of the optimla portfolio
+    muP = ExPort.mean()
+    sigP = calculate_sigma(ExPort)
+    SharpeP = np.sqrt(12) * muP / sigP
+    # print(ExPort.shape, Port.shape, rf.shape)  # double check the vectors are cpmpatible
+    annualized_mean = (1 + muP) ** 12 - 1  # annualized mean
+    annualized_std = sigP * np.sqrt(12)  # annualized std
+    print("\nOptimal Portfolio Metrics:")
+    print("Annualized Mean Return: {:.5f}".format(annualized_mean))
+    print("Annualized Standard Deviation: {:.5f}".format(annualized_std))
+    print("Annualized Sharpe Ratio: {:.5f}".format(SharpeP))
+    print("-" * 50)
+
+q_one_2()
